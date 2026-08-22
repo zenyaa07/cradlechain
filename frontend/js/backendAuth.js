cc28a0ddd519f080972be252c089d136f2fbfa89
zenyaa07 <zenyajong07@gmail.com>
2026-08-22 13:14:03 +0800

    Update API_BASE URL to production endpoint

--- frontend/js/backendAuth.js
+++ frontend/js/backendAuth.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "https://cradlechain-backend.onrender.com/api";
 
 export async function getCsrfToken() {
   await fetch(`${API_BASE}/auth/csrf/`, { credentials: "include" });
